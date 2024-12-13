5ec0a5e66dd2d7f7132daf83f5c2a77dbe0b3aa3
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2024-12-13 06:27:55 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+//using .mjs extentsion so Node.js can recognize the file as a module
+//NOTE: would use .mjs or flag with --experimental-modules in terminal
+
+const http = require('http'),
+ fs = require('fs'),
+ url = require('url');
+
+http.createServer((request, response) => {
+    let addr = request.url,
+    q = new URL(addr, 'http://' + request.headers.host),
+    filePath = '';
+
+fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+        console.error('Error writing to log.txt', err);
+    } else {
+        console.log('Added to log.')
+           }
+    });
+    
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        console.log('Bad URL, redireting to home');
+        filePath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
+
+}).listen(8080);
+console.log('My first node test server is runnning on port 8080');
+
+
+// 'log.txt', `URL: ${addr} \nTimestamp: ${new Date()} \n\n`, (err) => 
+
+// fs.appendFile('log.txt', 'Test log entry\n', (err) => {
+//     if (err) {
+//         console.error('Error writing test log entry:', err);
+//     } else {
+//         console.log('Test log entry added.');
+//     }
+// });
