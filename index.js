cf2fec1517a09f586bc124d56b345bd3049e3b97
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2025-01-15 05:53:02 -0800

    Implemented write stream for logging

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+
 const app = express();
 
-// app.use(morgan('common'));
-app.use(morgan('dev'));
+//create a write stream in append mode. This creates a log.txt file in the root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+app.use(morgan('common', {stream: accessLogStream}));
 
 app.get('/', (req, res) => {
-    res.send('Wecome Home log!')
+    res.send('Welcome Home log!')
 });
 
 app.get('/secreturl', (req, res)=> {
     res.send('secret cool awesomeness');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something Broke!');
 });
 
 app.listen(8080, () => {
