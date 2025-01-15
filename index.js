ab169cf17f5db9117f5d48a670e43c03dab187d9
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2025-01-15 09:55:10 -0800

    Created JSON list of movies
    Logs associate with get requests

--- index.js
+++ index.js
@@ -14,8 +14,80 @@ app.get('/', (req, res) => {
     res.send('Welcome Home log!')
 });
 
-app.get('/secreturl', (req, res)=> {
-    res.send('secret cool awesomeness');
+app.get('/movies', (req, res)=> {
+    const movieList = [
+         {
+            title: 'Django Unchained',
+            director: 'Quentin Tarantino',
+            Actors: 'Jamie Foxx, Christoph Waltz, Leonardo DiCaprio',
+            Genre: 'Dark Comedy, Drama, Western', 
+            Release: '2012'
+         },
+         {
+            title: 'There Will Be Blood',
+            director: 'Paul Thomas Anderson',
+            Actors: 'Daniel Day Lewis, Paul Dano, Ciaran Hinds',
+            Genre: 'Drama',
+            Release: '2007'
+         },
+         {
+            title: 'Once Upon a Time... in Hollywood',
+            director: 'Quentin Tarantino',
+            Actors: 'Leonardo DiCaprico, Brad Pitt, Margot Robbie',
+            Genre: 'Comedy, Drama',
+            Release: '2019'
+         },
+         {
+            title: 'Dunkirk',
+            director: 'Christopher Nolan',
+            Actors: 'Fionn Whitehead, Barry Keoghan, Mark Rylance',
+            Genre: 'Action , History',
+            Release: '2017'
+         },
+         {
+            title: 'No Country for Old Men',
+            director: 'Ethan Cohen, Joel Cohen',
+            Actors: 'Tommy Lee Jones, Javier Bardem, Josh Brolin',
+            Genre: 'Thriller, Drama',
+            Release: '2007'
+         },
+         {
+            title: "The Royal Tenenbaums",
+            director: 'Wes Anderson',
+            Actors: 'Gene Hackman, Gwyneth Paltrow, Anjelica Huston',
+            Genre: 'Comedy, Drama',
+            Release: '2001'
+         },
+         {
+            title: 'Inglorious Basterds',
+            director: 'Quentin Tarantino',
+            Actors: 'Brad Pitt, Diane Kruger, Eli Roth',
+            Genre: 'Dark Comedy, War',
+            Release: '2009'
+         },
+         {
+            title: 'Good Will Hunting',
+            director: 'Gus Van Zant',
+            Actors: 'Robin Williams, Matt Damon, Ben Affleck',
+            Genre: 'Drama, Romance',
+            Release: '1997'
+         },
+         {
+            title: '1917',
+            director: 'Sam Mendez',
+            Actors: 'Dean-Charles Chapman, George MacKay, Daniel Mays',
+            Genre: 'Action, War',
+            Release: '2019'
+         },
+         {
+            title: 'Darkest Hour',
+            director: 'Joe Wright',
+            Actors: 'Gary Oldman, Lily James, Kristin Scott Thomas', 
+            Genre: 'Drama, War',
+            Release: '2017'
+         },
+    ];
+    res.status(200).send(movieList);
 });
 
 app.use((err, req, res, next) => {
