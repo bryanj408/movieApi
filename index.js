50ab6359f4a9fba937e881185b086521e4c9befa
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2025-06-06 06:10:03 -0700

    Updated file to use schemas instead of in memory
    imported mongoose, models, express, bcrypt
    
    connected to localhost and app.use(express)
    
    created app.post /users

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express'),
-    morgan = require('morgan'),
-    fs = require('fs'),
-    path = require('path');
+// const express = require('express'),
+//     morgan = require('morgan'),
+//     fs = require('fs'),
+//     path = require('path');
+import mongoose from 'mongoose';
+import models from './models.js';
+import express from 'express';
+import bcrypt from 'bcrypt';
+
+const Npcs = models.Npc;
+const Users = models.User;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/movieApi', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //create a write stream in append mode. This creates a log.txt file in the root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
@@ -14,80 +25,50 @@ app.get('/', (req, res) => {
     res.send('Welcome Home log!')
 });
 
-app.get('/movies', (req, res)=> {
-    const movieList = [
-         {
-            title: 'Django Unchained',
-            director: 'Quentin Tarantino',
-            Actors: 'Jamie Foxx, Christoph Waltz, Leonardo DiCaprio',
-            Genre: 'Dark Comedy, Drama, Western', 
-            Release: '2012'
-         },
-         {
-            title: 'There Will Be Blood',
-            director: 'Paul Thomas Anderson',
-            Actors: 'Daniel Day Lewis, Paul Dano, Ciaran Hinds',
-            Genre: 'Drama',
-            Release: '2007'
-         },
-         {
-            title: 'Once Upon a Time... in Hollywood',
-            director: 'Quentin Tarantino',
-            Actors: 'Leonardo DiCaprico, Brad Pitt, Margot Robbie',
-            Genre: 'Comedy, Drama',
-            Release: '2019'
-         },
-         {
-            title: 'Dunkirk',
-            director: 'Christopher Nolan',
-            Actors: 'Fionn Whitehead, Barry Keoghan, Mark Rylance',
-            Genre: 'Action , History',
-            Release: '2017'
-         },
-         {
-            title: 'No Country for Old Men',
-            director: 'Ethan Cohen, Joel Cohen',
-            Actors: 'Tommy Lee Jones, Javier Bardem, Josh Brolin',
-            Genre: 'Thriller, Drama',
-            Release: '2007'
-         },
-         {
-            title: "The Royal Tenenbaums",
-            director: 'Wes Anderson',
-            Actors: 'Gene Hackman, Gwyneth Paltrow, Anjelica Huston',
-            Genre: 'Comedy, Drama',
-            Release: '2001'
-         },
-         {
-            title: 'Inglorious Basterds',
-            director: 'Quentin Tarantino',
-            Actors: 'Brad Pitt, Diane Kruger, Eli Roth',
-            Genre: 'Dark Comedy, War',
-            Release: '2009'
-         },
-         {
-            title: 'Good Will Hunting',
-            director: 'Gus Van Zant',
-            Actors: 'Robin Williams, Matt Damon, Ben Affleck',
-            Genre: 'Drama, Romance',
-            Release: '1997'
-         },
-         {
-            title: '1917',
-            director: 'Sam Mendez',
-            Actors: 'Dean-Charles Chapman, George MacKay, Daniel Mays',
-            Genre: 'Action, War',
-            Release: '2019'
-         },
-         {
-            title: 'Darkest Hour',
-            director: 'Joe Wright',
-            Actors: 'Gary Oldman, Lily James, Kristin Scott Thomas', 
-            Genre: 'Drama, War',
-            Release: '2017'
-         },
-    ];
-    res.status(200).send(movieList);
+//Add a user
+/*We'll expect JSON in this format
+{
+Username: String,
+Password: String,
+Email: String,
+Birthday: Date,
+}*/
+app.post('/users', async (req, res) => {
+    await Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + ' already exists');
+            } else {
+                //Basic input validation
+                if (!req.body.Username || !req.body.Password || !req.body.Email) {
+                    return res.status(400).send('All fields are required')
+                }
+
+                //Hash the Password
+                bcrypt.hash(req.body.Password, 10)
+                .then((hashedPassword) => {
+                    Users.create({
+                        Username: req.body.Username,
+                        Password: hashedPassword,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday
+                    })
+                    .then((user) => { res.status(201).json(user) })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error: ' + error);
+                    });
+                })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error: ' + error);
+                });
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
 });
 
 app.use((err, req, res, next) => {
