844b2a3ac56f2c582127f2a573eeece896ce503b
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2025-06-19 14:20:56 -0700

    proper import of mongoose models
    I was exporting 'default' {Npc, Usert} but not defining models. Simplified this by default exporting properly

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
-import { Npc, User } from './models.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
